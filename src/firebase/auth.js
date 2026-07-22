808885b4f6ff97b80ca71129a5d3e0702655c535
lucio77-cd <luciosantoscarvalho@gmail.com>
2026-07-22 12:54:34 -0300

    Implement Firebase authentication functions

---
+++ src/firebase/auth.js
@@ -0,0 +1,22 @@
+// src/firebase/auth.js
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  signOut,
+  createUserWithEmailAndPassword,
+} from "firebase/auth";
+import { app } from "./config";
+
+export const auth = getAuth(app);
+
+export function login(email, senha) {
+  return signInWithEmailAndPassword(auth, email, senha);
+}
+
+export function logout() {
+  return signOut(auth);
+}
+
+export function cadastrar(email, senha) {
+  return createUserWithEmailAndPassword(auth, email, senha);
+}
