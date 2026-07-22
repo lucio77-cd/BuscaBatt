6f0e8003a4b7117b19ffe749def9990851d91273
lucio77-cd <luciosantoscarvalho@gmail.com>
2026-07-22 13:03:15 -0300

    Add usuarioService for user management functions

---
+++ src/servicos/usuarioService.js
@@ -0,0 +1,12 @@
+// src/servicos/usuarioService.js
+import { doc, getDoc, setDoc } from "firebase/firestore";
+import { db } from "../firebase/firestore";
+
+export async function buscarUsuario(uid) {
+  const snap = await getDoc(doc(db, "usuarios", uid));
+  return snap.exists() ? snap.data() : null;
+}
+
+export function criarUsuario(uid, dados) {
+  return setDoc(doc(db, "usuarios", uid), { ...dados });
+}
