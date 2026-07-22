30360337ccdc45b8157a87a8b22a371d15a4e698
lucio77-cd <luciosantoscarvalho@gmail.com>
2026-07-22 13:02:10 -0300

    Add useAuth hook for authentication context

---
+++ src/hooks/useAuth.js
@@ -0,0 +1,9 @@
+// src/hooks/useAuth.js
+import { useContext } from "react";
+import { AuthContext } from "../contextos/AuthContext";
+
+export function useAuth() {
+  const contexto = useContext(AuthContext);
+  if (!contexto) throw new Error("useAuth deve estar dentro de AuthProvider");
+  return contexto;
+}
